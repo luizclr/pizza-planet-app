66ef655c614956b992334347129a8eb5d913bdd9
Luiz Claudio <luiz.lclr@gmail.com>
2023-05-24 23:50:49 -0300

    feat(env): setup env variables

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   verbose: true,
   preset: "react-native",
   roots: ["<rootDir>/tests"],
+  setupFiles: ["./tests/setup-envs.js"],
   setupFilesAfterEnv: ["./tests/setup.js"],
   collectCoverageFrom: ["src/**/*.{ts,tsx}"],
   testMatch: ["<rootDir>/tests/**/*.{spec,test}.{ts,tsx}"],
